75d86cc31166abbc029d3c8370675a1027ee951c
sebacozzi <127224339+sebacozzi@users.noreply.github.com>
2023-03-15 20:02:34 -0300

    Actualización de ejercicio 11 Manos a la Obra

--- MalO/Ejercicio_11/src/ejercicio_11/Ejercicio_11.java
+++ MalO/Ejercicio_11/src/ejercicio_11/Ejercicio_11.java
@@ -1,16 +1,17 @@
-/**Escribir un programa que procese una secuencia de caracteres ingresada por 
-* teclado y terminada en punto, y luego codifique la palabra o frase ingresada 
-* de la siguiente manera: cada vocal se reemplaza por el carácter que se indica 
-* en la tabla y el resto de los caracteres (incluyendo a las vocales acentuadas)
-* se mantienen sin cambios.
-*    a e i o u
-*    @ # $ % *
-*
-* Realice un subprograma que reciba una secuencia de caracteres y retorne la 
-* codificación correspondiente. Utilice la estructura “según” para la 
-* transformación. 
-* Por ejemplo, si el usuario ingresa:     Ayer, lunes, salimos a las once y 10.
-* La salida del programa debería ser:  @y#r, l*n#s, s@l$m%s @ l@s %nc# y 10.
+/** Escribir un programa que procese una secuencia de caracteres ingresada por
+ * teclado y terminada en punto, y luego codifique la palabra o frase ingresada
+ * de la siguiente manera: cada vocal se reemplaza por el carácter que se indica
+ * en la tabla y el resto de los caracteres (incluyendo a las vocales acentuadas)
+ * se mantienen sin cambios.
+ *    a e i o u
+ *
+ * @ # $ % *
+ *
+ * Realice un subprograma que reciba una secuencia de caracteres y retorne la
+ * codificación correspondiente. Utilice la estructura “según” para la
+ * transformación. Por ejemplo, si el usuario ingresa: Ayer, lunes, salimos a
+ * las once y 10. La salida del programa debería ser: @y#r, l*n#s, s@l$m%s @ l@s
+ * %nc# y 10.
  */
 package ejercicio_11;
 
@@ -32,16 +33,15 @@ public class Ejercicio_11 {
     }
 
     private static String cambioLetras(String frase) {
-
         String temp = "";
         String letra;
         for (int i = 0; i < frase.length(); i++) {
             switch (frase.substring(i, i + 1)) {
                 case "a", "A":
-                    letra= "@";
+                    letra = "@";
                     break;
                 case "e", "E":
-                    letra="#";
+                    letra = "#";
                     break;
                 case "i", "I":
                     letra = "$";
@@ -53,9 +53,9 @@ public class Ejercicio_11 {
                     letra = "*";
                     break;
                 default:
-                    letra= frase.substring(i, i+1);
+                    letra = frase.substring(i, i + 1);
             }
-            temp=temp.concat(letra);
+            temp = temp.concat(letra);
         }
         return temp;
     }
